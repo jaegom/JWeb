20317cb5944974f6d22a393b4984e4dcd8176f51
jaegom <46448300+jaegom@users.noreply.github.com>
2022-12-15 15:19:45 +0900

    Successfully deployed on Oracle Cloud

--- JWeb/src/main/java/com/jweb/sbb/JWebApplication.java
+++ JWeb/src/main/java/com/jweb/sbb/JWebApplication.java
@@ -2,9 +2,12 @@ package com.jweb.sbb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+// extends SpringBootServletInitializer // 기본 Runtime에 내장 톰캣을 쓰지 않겠다.
 
 @SpringBootApplication
-public class JWebApplication {
+public class JWebApplication { 
 
 	public static void main(String[] args) {
 		SpringApplication.run(JWebApplication.class, args);
