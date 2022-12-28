a56a8c4842b27af0607b774f9922b4f779f5f142
jaegom <46448300+jaegom@users.noreply.github.com>
2022-12-28 16:49:19 +0900

    unnecessary quotation
    servlet initializer quotation

--- JWeb/src/main/java/com/jweb/sbb/JWebApplication.java
+++ JWeb/src/main/java/com/jweb/sbb/JWebApplication.java
@@ -3,18 +3,10 @@ package com.jweb.sbb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
-// extends SpringBootServletInitializer // 기본 Runtime에 내장 톰캣을 쓰지 않겠다.
 
 @SpringBootApplication
 public class JWebApplication {
-/*	
-	@Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(JWebApplication.class);
-    }
-*/
+
 	public static void main(String[] args) {
 		SpringApplication.run(JWebApplication.class, args);
 	}
