3b51ee277241d66209b7c7de4801d60311e62f3c
jaegom <46448300+jaegom@users.noreply.github.com>
2023-01-20 11:24:21 +0900

    fetch

--- JWeb/src/main/java/com/jweb/sbb/user/UserRepository.java
+++ JWeb/src/main/java/com/jweb/sbb/user/UserRepository.java
@@ -3,5 +3,6 @@ package com.jweb.sbb.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<SiteUser, Long> {
-
+	
+	
 }
---
+++ JWeb/src/main/java/com/jweb/sbb/user/UserSecurityService.java
@@ -0,0 +1,5 @@
+package com.jweb.sbb.user;
+
+public class UserSecurityService {
+
+}
