296b276b6ba0c747d21edfb6a29af1eb39a87dbd
jaegom <p.jaeshik@gmail.com>
2023-01-23 14:51:44 +0900

    SPRING BOOT SECURITY 3.1.1

--- JWeb/src/test/java/com/jweb/sbb/JWebApplicationTests.java
+++ JWeb/src/test/java/com/jweb/sbb/JWebApplicationTests.java
@@ -38,7 +38,7 @@ class JWebApplicationTests {
 	@Test
 	void testJpa() {
 		
-		for (int i = 1; i <= 300; i++) {
+		for (int i = 1; i <= 10; i++) {
 	        String subject = String.format("테스트 데이터입니다:[%03d]", i);
 	        String content = "내용무";
 	        this.questionService.create(subject, content);
