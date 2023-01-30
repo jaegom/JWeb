c6f46023f94499354d95426086c9618feec1f61d
jaegom <46448300+jaegom@users.noreply.github.com>
2023-01-30 20:57:23 +0900

    search
    search by query

--- JWeb/src/main/java/com/jweb/sbb/question/QuestionController.java
+++ JWeb/src/main/java/com/jweb/sbb/question/QuestionController.java
@@ -27,18 +27,17 @@ import lombok.RequiredArgsConstructor;
 
 /**
  * @RequiredArgsConstructor
- * @author 바이텍씨스템
- * 
- *         questionRepository 속성을 포함하는 생성자를 생성
- * @RequiredArgsConstructor는 롬복이 제공하는 애너테이션으로 final이 붙은 속성을 포함하는 생성 자를 자동으로 생성하는
- *                           역할을 한다. 롬복의 @Getter, @Setter가 자동으로 Getter, Setter
- *                           메서드를 생성 하는 것과 마찬가지로 @RequiredArgsConstructor는 자동으로
- *                           생성자를 생성한다. 따라서 스프링 의존성 주입 규칙에 의해 questionRepository
- *                           객체가 자동으로 주입된다.
- * 
- *                           의존성 주입 방법 중 생성자를 작성하여 객체를 주입하는 방식에 해당된다 (자동으로 객체
- *                           lombok이 생성해줌)
- * 
+ * @author 바이텍씨스템  
+ * @RequiredArgsConstructor는 
+ */
+
+/*
+ * questionRepository 속성을 포함하는 생성자를 생성
+ * 롬복이 제공하는 애너테이션으로 final이 붙은 속성을 포함하는 생성 자를 자동으로 생성하는 역할을 한다.
+ * 롬복의 @Getter, @Setter가 자동으로 Getter, Setter 메서드를 생성 하는 것과
+ * 마찬가지로 @RequiredArgsConstructor는 자동으로 생성자를 생성한다. 따라서 스프링 의존성 주입 규칙에 의해
+ * questionRepository 객체가 자동으로 주입된다.
+ * 의존성 주입 방법 중 생성자를 작성하여 객체를 주입하는 방식에 해당된다 (자동으로 객체 lombok이 생성해줌)
  */
 
 @RequestMapping("/question")
@@ -95,9 +94,14 @@ public class QuestionController {
 
 	@GetMapping("/list")
 	// @ResponseBody
-	public String list(Model model, @RequestParam(value = "page", defaultValue = "0") int page) {
-		Page<Question> paging = this.questionService.getList(page);
+	public String list(Model model, 
+			@RequestParam(value = "page", defaultValue = "0") int page, 
+			@RequestParam(value="kw", defaultValue="") String kw) {
+		Page<Question> paging = this.questionService.getList(page, kw);
 		model.addAttribute("paging", paging);
+		
+		//검색어 화면에 유지
+		model.addAttribute("kw", kw);
 		return "thymeleaf/question_list";
 	}
 	
